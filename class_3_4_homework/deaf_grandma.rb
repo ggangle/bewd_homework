00097a10194b14d58089b9c3e47e8cc58d0ef6fe
Greta Gangle <greta.gangle@gmail.com>
2015-04-08 13:35:18 -0700

    finished assignment

--- class_3_4_homework/deaf_grandma.rb
+++ class_3_4_homework/deaf_grandma.rb
@@ -1,12 +1,13 @@
 grandma = true
-year = rand(1920..1980)
+year = rand(1930..1950)
 
 while grandma
 puts "What would you like to tell grandma?"
 answer = gets.chomp
-	if answer == answer.upcase
-		puts "NO, NOT SINCE #{year}!"
+	if answer == "BYE"
 		grandma = false
+	elsif answer == answer.upcase
+		puts "NO, NOT SINCE #{year}!"
 	else 
 		puts "HUH?! SPEAK UP, SONNY!"
 	end
