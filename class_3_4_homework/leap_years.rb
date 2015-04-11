97ba61af5896d6126eae09f8ce1360e5f775e480
Greta Gangle <greta.gangle@gmail.com>
2015-04-11 14:20:03 -0700

    had a loop in there that didn't belong

--- class_3_4_homework/leap_years.rb
+++ class_3_4_homework/leap_years.rb
@@ -1,9 +1,7 @@
-years = true
-
-	puts "What is your starting year?"
-	starting_year = gets.chomp.to_i
-	puts  "What is your ending year?"
-	ending_year = gets.chomp.to_i
+puts "What is your starting year?"
+starting_year = gets.chomp.to_i
+puts  "What is your ending year?"
+ending_year = gets.chomp.to_i
 
 starting_year.upto(ending_year) do |year|
 	if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
